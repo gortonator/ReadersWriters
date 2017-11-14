9cf0b0e5ba9f50baa91fc33cdf0a6fa30db5b37b
gortonator <gortonator@outlook.com>
2017-11-13 18:54:19 -0800

    added files

---
+++ ReadersWriters.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package readerswriters;
+
+/**
+  This app creates a specified number of readers and 
+  writers and starts them.
+*/
+public class ReadersWriters
+{
+  /**
+    Creates the specified number of readers and writers and starts them.
+ 
+    @param args[0] The number of readers.
+    @param args[1] The number of writers.
+  */
+  public static void main(String[] args)
+  {
+    if (args.length < 2)
+    {
+      System.out.println("Usage: java Simulator <number of readers> <number of writers>");
+    }
+    else
+    {
+      final int READERS = Integer.parseInt(args[0]);
+      final int WRITERS = Integer.parseInt(args[1]);
+      Database database = new Database();
+      for (int i = 0; i < READERS; i++)
+      {
+        new Reader(database).start();
+      }
+      for (int i = 0; i < WRITERS; i++)
+      {
+        new Writer(database).start();
+      }
+    }
+  }
+}
